a31f8e32b52ed46fb8e396d366b9bc12f53df7a4
Dinh Phu Nguyen <phund30@fpt.com>
2024-02-02 16:54:15 +0700

    Fix install_zsh:
    Error when mkdir fonts_dir

--- install_zsh.sh
+++ install_zsh.sh
@@ -29,7 +29,7 @@ install_fonts() {
   fonts_dir="#$HOME/.local/share/fonts"
   if [ ! -d "${fonts_dir}" ]; then
     echo "mkdir -p ${fonts_dir}"
-    mkdir -p "${fonts_dir}"
+    mkdir -p ${fonts_dir}
   else
     echo "Found fonts dir ${fonts_dir}"
   fi
