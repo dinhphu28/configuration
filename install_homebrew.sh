64c1960113bb403cb41e7a92a1cda63f98daec88
Dinh Phu Nguyen <phund30@fpt.com>
2024-02-02 15:50:14 +0700

    Update install_brew:
    Change home path -> $HOME

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -3,7 +3,7 @@ install_brew() {
   echo "Installing homebrew..."
   if test ! $(which brew); then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/dinhphu28/.zshrc
+    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.zshrc
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     # For fedora
     sudo yum groupinstall 'Development Tools'
