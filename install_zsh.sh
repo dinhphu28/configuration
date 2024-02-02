d52be7059b1589509498ca11e58227bce5117e11
Dinh Phu Nguyen <phund30@fpt.com>
2024-02-02 16:48:58 +0700

    Update install_zsh:
    Add method install_fonts

--- install_zsh.sh
+++ install_zsh.sh
@@ -23,6 +23,33 @@ install_zsh_auto_suggestions() {
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
+install_fonts() {
+  echo "Downloading and installing MesloLGS NF fonts..."
+
+  fonts_dir=#$HOME/.local/share/fonts"
+  if [ ! -d "${fonts_dir}" ]; then
+    echo "mkdir -p ${fonts_dir}"
+    mkdir -p "${fonts_dir}"
+  else
+    echo "Found fonts dir ${fonts_dir}"
+  fi
+
+  for type in "Bold" "Bold Italic" "Italic" "Regular"; do
+    file_path="$HOME/.local/share/fonts/MesloLGS\ NF\ ${type}.ttf"
+    file_url="https://github.com/romkatv/powerlevel10k-media/blob/master/MesloLGS NF ${type}.ttf"
+    if [ ! -e "${file_path}" ]; then
+      echo "wget -O ${file_path} ${file_url}"
+      wget -O "${file_path}" "${file_url}"
+    else
+      echo "Found existing file ${file_path}"
+    fi;
+  done
+
+  echo "fc-cache -f"
+  fc-cache -f
+  echo "MesloLGS NF installed"
+}
+
 install_zsh_powerlevel10k() {
   echo "Installing powerlevel10k..."
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
