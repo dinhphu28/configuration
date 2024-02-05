5f0dda695faa6339abd7efb746f0f0d64b727627
Dinh Phu Nguyen <phund30@fpt.com>
2024-02-05 11:31:47 +0700

    Comment out install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-chmod +x ./**.sh
-source ./*.sh
-source internal/**.sh
-
-# TODO: Should switch to zsh after installation
-install_zsh
-
-install_oh_my_zsh
-install_zsh_syntax_highlighting
-install_zsh_auto_suggestions
-install_zshpowerlevel10k
-
-install_brew
-install_docker
-./brew_packages/install.sh
-
-update_dotfiles
+# chmod +x ./**.sh
+# source ./*.sh
+# source internal/**.sh
+#
+# # TODO: Should switch to zsh after installation
+# install_zsh
+#
+# install_oh_my_zsh
+# install_zsh_syntax_highlighting
+# install_zsh_auto_suggestions
+# install_zshpowerlevel10k
+#
+# install_brew
+# install_docker
+# ./brew_packages/install.sh
+#
+# update_dotfiles
