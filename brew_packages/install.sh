5c8726a30d5b89e84d936fa3e19934c5ca5d69e1
Dinh Phu Nguyen <phund30@fpt.com>
2024-02-05 11:52:57 +0700

    Update install.sh

--- brew_packages/install.sh
+++ brew_packages/install.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
-echo "Installing brew util applications..."
-brew install neovim
-brew install gh
-brew install ghq
-brew install nmap
-brew install fzf
-$(brew --prefix)/opt/fzf/install
-brew install fd
-brew install ripgrep
-brew install neofetch
+install_brew_packages() {
+  echo "Installing brew util applications..."
+  brew install neovim
+  brew install gh
+  brew install ghq
+  brew install nmap
+  brew install fzf
+  $(brew --prefix)/opt/fzf/install
+  brew install fd
+  brew install ripgrep
+  brew install neofetch
 
-echo "Installing brew dev applications..."
-brew install npm
-brew install yarn
+  echo "Installing brew dev applications..."
+  brew install npm
+  brew install yarn
 
-brew install openjdk
-brew install openjdk@11
-brew install openjdk@17
-brew install maven
-brew install jenv
+  brew install openjdk
+  brew install openjdk@11
+  brew install openjdk@17
+  brew install maven
+  brew install jenv
 
-brew install coursier/formulas/coursier
-echo "Installing scala..."
-coursier setup
-brew install metals
+  brew install coursier/formulas/coursier
+  echo "Installing scala..."
+  coursier setup
+  brew install metals
 
-brew install go
+  brew install go
+}
--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-# chmod +x ./**.sh
-# source ./*.sh
-# source internal/**.sh
-#
-# # TODO: Should switch to zsh after installation
-# install_zsh
-#
-# install_oh_my_zsh
-# install_zsh_syntax_highlighting
-# install_zsh_auto_suggestions
-# install_zshpowerlevel10k
-#
-# install_brew
-# install_docker
-# ./brew_packages/install.sh
-#
-# update_dotfiles
+# import all shell scripts
+. ./internal/helpers.sh
+. ./internal/update_dotfiles.sh
+. ./install_zsh.sh
+. ./install_homebrew.sh
+. ./install_docker.sh
+. ./brew_packages/install.sh
+
+ # TODO: Should switch to zsh after installation
+ install_zsh
+
+install_oh_my_zsh
+install_zsh_syntax_highlighting
+install_zsh_auto_suggestions
+install_zshpowerlevel10k
+
+install_brew
+install_docker
+./brew_packages/install.sh
+
+update_dotfiles
